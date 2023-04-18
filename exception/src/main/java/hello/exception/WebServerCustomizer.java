aaeb5df93209f45c015602fc17c14deb6e3d3c01
power30111_HOME <power30111@naver.com>
2023-04-19 08:16:15 +0900

    Servlet Exception (filter, Interceptor) ,Spring boot -> error page config

---
+++ exception/src/main/java/hello/exception/WebServerCustomizer.java
@@ -0,0 +1,22 @@
+package hello.exception;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+//@Component
+public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
+        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
+        //HTTP 상태코드 404, 500 일때 에러페이지 정의
+        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class,"/error-page/500");
+        //Runtime Exception 일때 에러페이지( Runtime Exception의 자식들 또한 포함)
+        factory.addErrorPages(errorPage404,errorPage500,errorPageEx);
+
+    }
+}
